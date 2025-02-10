02e53ca464308dd59c8f04109a03bd5ab5c195cc
devkanix <opkanix@gmail.com>
2025-02-10 13:56:03 +0530

    `Added dropdown menu to Navbar component and updated Products page to include Navbar and Footer components.`

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
   return (
     <div className="flex justify-around mt-5 mb-5">
       <h1 className="text-xs font-bold text-white mt-7 sm:mt-4 sm:text-4xl">
@@ -9,8 +10,30 @@ export const Navbar = () => {
       </h1>
       <div className="flex gap-2 sm:gap-9 mt-5">
         <div className="flex text-xs sm:text-lg text-white list-none gap-2 sm:gap-9 mt-2 sm:mt-0.5 ">
-          <li className="transition-text duration-200 hover:text-yellow-300">
-            <Link to="categories">CATEGORIES</Link>
+          <li className="transition-text duration-200 hover:text-yellow-300 cursor-pointer"
+          onMouseEnter={() => setShowDropdown(true)}
+          onMouseLeave={() => setShowDropdown(false)}>
+            CATEGORIES
+            {showDropdown && (
+                <div className="absolute mt-2">
+                  <div className="absolute -top-2 left-4 w-0 h-0 
+                    border-l-[8px] border-l-transparent 
+                    border-b-[8px] border-b-white/30 
+                    border-r-[8px] border-r-transparent">
+                  </div>
+                  <ul className="backdrop-blur-md bg-white/30 w-[100px] p-1 rounded-lg">
+                    <li className="text-white transition-text duration-200 hover:text-yellow-400">
+                      <Link to="mens">Mens</Link>
+                    </li>
+                    <li className="text-white transition-text duration-200 hover:text-yellow-400">
+                      <Link to="womens">Womens</Link>
+                    </li>
+                    <li className="text-white transition-text duration-200 hover:text-yellow-400">
+                      <Link to="kids">Kids</Link>
+                    </li>
+                  </ul>
+                </div>
+            )}
           </li>
           <li className="transition-text duration-200 hover:text-yellow-300">
             <Link to="fashion">FASHION</Link>
@@ -34,4 +57,4 @@ export const Navbar = () => {
     </div>
   );
 };
-export default Navbar;
+export default Navbar;
--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import photo from "../Assets/front-2.png";
 
 const Products = () => {
   return (
     <>
-        
+      <Navbar />
+      <div className="flex flex-col justify-center items-center">
+        <div className="w-[70%] mt-10">
+          <h1 className="text-white text-5xl font-bold">Products</h1>
+          <div className="text-white mt-5 text-3xl font-semibold mb-10">
+              <div className="font-poppins mt-3">
+                <div>
+                  <img
+                    src={photo}
+                    alt="product"
+                    className="object-cover w-[400px] h-[550px] rounded-[20px]"
+                  />
+                </div>
+                <div className="flex justify-between">
+                  <div className="flex flex-col mt-2">
+                    <h3 className="font-medium text-2xl leading-10">Oversized T-Shirts</h3>
+                    <p className="text-lightWhite">Explore Now!</p>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
