95c34203c7c7df210e6c1b61afe241eda54ab3f3
Aman Pratap Singh <amanpratapsingh030@gmail.com>
2025-02-24 11:40:37 +0530

    Added OmniAuth feature with the JWT implementation.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import passport from 'passport';
+import session from 'express-session';
 
 import {PORT} from './config/serverConfig.js';
 import { dbconnect } from './config/database.js';
 import apiRoutes from './routes/index.js';
-import { passportAuth } from './config/jwt-middleware.js';
+import passportAuth from './config/jwt-middleware.js';
 
 const startAndStopServer = async () => {
     const app = express();
@@ -12,6 +13,16 @@ const startAndStopServer = async () => {
     //Adding middlewares
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+
+    // Middleware for session handling
+    app.use(session({
+        secret: 'your_secret_key',
+        resave: false,
+        saveUninitialized: false
+    }));
+    
+    app.use(passport.initialize());
+    app.use(passport.session());
 
     //passport authentication initilaztion
     app.use(passport.initialize());
